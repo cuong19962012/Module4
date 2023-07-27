2b01a51711ddc7c03613c1217defc98fa38898db
cuong19962012 <tancuong.it@gmail.com>
2023-07-28 00:07:55 +0700

    bai tap ss8

--- ss8_validation/excercise/excercise2/src/main/java/com/example/excercise2/controller/MusicController.java
+++ ss8_validation/excercise/excercise2/src/main/java/com/example/excercise2/controller/MusicController.java
@@ -1,14 +1,39 @@
 package com.example.excercise2.controller;
 
+import com.example.excercise2.model.MusicDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class MusicController {
     @GetMapping("/")
     public ModelAndView home() {
-        re
+        return new ModelAndView("register", "musicDto", new MusicDto());
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(@Valid @ModelAttribute MusicDto musicDto, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        new MusicDto().validate(musicDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("register");
+            modelAndView.addObject("musicDto", musicDto);
+            return modelAndView;
+        }
+        modelAndView.setViewName("show");
+        modelAndView.addObject("musicDto", musicDto);
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView edit(MusicDto musicDto) {
+        System.out.println(musicDto);
+        return new ModelAndView("edit", "musicDto", musicDto);
     }
 }
--- ss8_validation/excercise/excercise2/src/main/java/com/example/excercise2/model/MusicDto.java
+++ ss8_validation/excercise/excercise2/src/main/java/com/example/excercise2/model/MusicDto.java
@@ -1,6 +1,9 @@
 package com.example.excercise2.model;
 
-public class MusicDto {
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class MusicDto implements Validator {
     private String nameMusic;
     private String singer;
     private String musicGenre;
@@ -38,4 +41,47 @@ public class MusicDto {
         this.musicGenre = musicGenre;
     }
 
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        MusicDto musicDto = (MusicDto) target;
+
+        if (musicDto.getNameMusic().equals("")) {
+            errors.rejectValue("nameMusic", null, "Not Empty");
+        } else if (!musicDto.getNameMusic().matches("^\\w{1,}$")) {
+            errors.rejectValue("nameMusic", null, "Contains Special Characters");
+        }
+        if (musicDto.getNameMusic().length() > 800) {
+            errors.rejectValue("nameMusic", null, "Over 800 words");
+        }
+        if (musicDto.getSinger().equals("")) {
+            errors.rejectValue("singer", null, "Not Empty");
+        } else if (!musicDto.getSinger().matches("^\\w{1,}$")) {
+            errors.rejectValue("singer", null, "Contains Special Characters");
+        }
+        if (musicDto.getSinger().length() > 300) {
+            errors.rejectValue("singer", null, "Over 300 words");
+        }
+        if (musicDto.getMusicGenre().equals("")) {
+            errors.rejectValue("musicGenre", null, "Not Empty");
+        } else if (!musicDto.getMusicGenre().matches("^[a-zA-Z_0-9_,]{1,}$")) {
+            errors.rejectValue("musicGenre", null, "',' except Contains Special Characters");
+        }
+        if (musicDto.getMusicGenre().length() > 1000) {
+            errors.rejectValue("musicGenre", null, "Over 1000 words");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MusicDto{" +
+                "nameMusic='" + nameMusic + '\'' +
+                ", singer='" + singer + '\'' +
+                ", musicGenre='" + musicGenre + '\'' +
+                '}';
+    }
 }
